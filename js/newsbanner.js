f13c404d95739ef6b306cd8d2d2f2448989a956a
Anthony Kolber <ae@aestheticallyloyal.com>
2011-09-07 16:24:33 +1000

    Made a mobile-enabled, responsive version of the banner * Only CSS/JS updates required to enable on old sites

--- js/newsbanner.js
+++ js/newsbanner.js
@@ -1,11 +1,47 @@
+/*
+ * jQuery throttle / debounce - v1.1 - 3/7/2010
+ * http://benalman.com/projects/jquery-throttle-debounce-plugin/
+ * 
+ * Copyright (c) 2010 "Cowboy" Ben Alman
+ * Dual licensed under the MIT and GPL licenses.
+ * http://benalman.com/about/license/
+ */
+(function(b,c){var $=b.jQuery||b.Cowboy||(b.Cowboy={}),a;$.throttle=a=function(e,f,j,i){var h,d=0;if(typeof f!=="boolean"){i=j;j=f;f=c}function g(){var o=this,m=+new Date()-d,n=arguments;function l(){d=+new Date();j.apply(o,n)}function k(){h=c}if(i&&!h){l()}h&&clearTimeout(h);if(i===c&&m>e){l()}else{if(f!==true){h=setTimeout(i?k:l,i===c?e-m:e)}}}if($.guid){g.guid=j.guid=j.guid||$.guid++}return g};$.debounce=function(d,e,f){return f===c?a(d,e,false):a(d,f,e!==false)}})(this);
+
 $(function() {
 
-  var newsbanner = $('#newsbanner'),
+  var ratio = 0,
+      newsbanner = $('#newsbanner'),
       wrapper = $('#banner-wrapper'),
       banners = $('.banner', wrapper),
-      bannerHeight = banners.first().height(),
       links = $('ol a', newsbanner);
 
+  var resizeBanners = function() {
+    var bannerHeight = $('#newsbanner .banner img')[0].offsetHeight;
+    // The dimensions for all of the banner sub-elements are defined in ems,
+    // so we can resize everything in scale by setting the font-size property
+    // on #newsbanner (the parent)
+    $('#newsbanner').css('font-size', Math.floor(bannerHeight / 1.5) + '%');
+    // The wrapper overflow sizes need to be set manually to match the image
+    $('#banner-clip', newsbanner).css('height', bannerHeight);
+    wrapper.css('height', bannerHeight);
+  }
+
+  var imageLoad = function() {
+    // Calculate the image ratio (1:3, etc)
+    ratio = ((this.offsetHeight + 1) / this.offsetWidth) * 100;
+    // Resize banners (the first time)
+    resizeBanners();
+  }
+
+  // Once the image has loaded, we can reference its width and height
+  var image = $('#newsbanner .banner img').first();
+  // Reset the src to ensure the load event fires
+  image.load(imageLoad).attr('src', image.attr('src'));
+  // Resize the banners on page resize (throttled to run every 50ms)
+  $(window).resize( $.throttle(50, resizeBanners) );
+
+  // Tab-based navigation
   $('> a', banners).focus(function() {
     // store the index of the focused banner
     var index = banners.index(this.parentNode);
@@ -14,14 +50,14 @@ $(function() {
   });
 
   links.click(function(e) {
+    // prevent the default link action
+    e.preventDefault();
     // store the index of the clicked link
     var index = $(links).index(this);
     // add .selected to the current li, and remove it from all others
     $(this).parent().addClass('selected').siblings().removeClass('selected');
     // use the stored index to animate #banner-wrapper down the correct amount
-    wrapper.animate({ marginTop: bannerHeight * -index }, 750);
-    // prevent the default link action
-    e.preventDefault();
+    wrapper.animate({ marginTop: (-index * ratio) + '%' }, 750);
      // select the first nav item on page load
   }).first().parent().addClass('selected');
 
