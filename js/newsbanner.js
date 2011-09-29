316b1cb4f6871f7f4dcf7c9ded664e6544eb1616
Anthony Kolber <ae@aestheticallyloyal.com>
2011-09-29 16:34:31 +1000

    Hotfix a jquery bug
    Only occurs in IE8, when animating a property by negative percentage.

--- js/newsbanner.js
+++ js/newsbanner.js
@@ -8,6 +8,17 @@
  */
 (function(b,c){var $=b.jQuery||b.Cowboy||(b.Cowboy={}),a;$.throttle=a=function(e,f,j,i){var h,d=0;if(typeof f!=="boolean"){i=j;j=f;f=c}function g(){var o=this,m=+new Date()-d,n=arguments;function l(){d=+new Date();j.apply(o,n)}function k(){h=c}if(i&&!h){l()}h&&clearTimeout(h);if(i===c&&m>e){l()}else{if(f!==true){h=setTimeout(i?k:l,i===c?e-m:e)}}}if($.guid){g.guid=j.guid=j.guid||$.guid++}return g};$.debounce=function(d,e,f){return f===c?a(d,e,false):a(d,f,e!==false)}})(this);
 
+/*
+  jQuery hotfix
+  Addresses a bug in jQuery which causes an error when animating
+  negative percentages in IE8
+*/
+var oldcur = jQuery.fx.prototype.cur;
+jQuery.fx.prototype.cur = function() {
+  var cur = oldcur.apply(this, arguments);
+  return (cur || 1);
+}
+
 $(function() {
 
   var ratio = 0,
